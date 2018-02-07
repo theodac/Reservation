695238136f9d9e16d6555dd8adcca28c381c91ff
Théo Da Costa <32874437+theodac@users.noreply.github.com>
2018-02-07 18:04:19 +0100

    07/02

--- index.php
+++ index.php
@@ -1,41 +1,50 @@
 <?php
+include ('./functions/connexionPDO.php');
+$db = connectionPDO();
 
-include_once ("functions/connectionPDO.php");
 ?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Réservation de salle</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
-
 <body>
-<h2>Réservation de salle</h2>
-<form method="post" action="reservation.php">
-    <select name="salle">
-        <option value="0">Choix salle</option>
-        <?php
-        $db=connectionPDO('localhost' , 'reservation' , 'root' , '');
-        $requete = "SELECT * FROM t_rooms";
-        $result = $db -> query($requete);
-        while($donnees=$result->fetch()) {
+<form method="post" action="reservations.php">
+<?php
+$sql =  "SELECT * FROM t_salles ";
+$reponse = $db->query($sql);
+echo "<select name='select'>";
+while ($donnees = $reponse->fetch(PDO::FETCH_ASSOC)){
+    echo "<option value='".$donnees['ID_SALLE'] . "'> ".$donnees['SALLENOM'] . "</option>";
+}
+echo "</select>";
 
-            echo("<option value=" . $donnees['ID_ROOMS'].">" .$donnees['ROOMNAME'] . "</option>");
-
-
-        }
-        ?>
-    </select>
-    <input type="datetime-local" name="date">
-    <select name="duree">
-        <option value="0">Choix durée</option>
-        <option value="1">1h</option>
-        <option value="2">2h</option>
-        <option value="3">3h</option>
-        <option value="4">4h</option>
-
-    </select>
-    <input type="submit" value="Reserver" name="reservation">
+?>
+    <br>
+    <label>Nombre de participants : </label>
+    <input type="number" name="participant">
+    <br>
+    <label>Date de la réservations : </label>
+    <input type="date" name="date">
+    <br>
+    <label>Heure de début : </label>
+    <input type="time" name="heureD">
+    <br>
+    <label>Heure de Fin : </label>
+    <input type="time" name="heureF">
+    <br>
+    <label>Votre Nom : </label>
+    <input type="text" name="nom">
+    <br>
+    <input type="submit" name="envoi">
 </form>
+
 </body>
 </html>
---
+++ reservations.php
@@ -0,0 +1,41 @@
+<?php
+include ('./functions/connexionPDO.php');
+if (isset($_POST['envoi'])){
+    $db = connectionPDO();
+
+    $nbrParticipants = $_POST['participant'];
+    $dateReservation = $_POST['date'];
+    $heureDebut = $_POST['heureD'];
+    $heurefin = $_POST['heureF'];
+    $salle = $_POST['select'];
+    $nom = $_POST['nom'];
+
+
+    $sql = "SELECT * FROM t_salles ";
+    $reponse = $db->query($sql);
+    while ($donnees = $reponse->fetch(PDO::FETCH_ASSOC)){
+        if($nbrParticipants > $donnees['SALLEPLACE']){
+
+            echo "Veuillez choisir une autre salle , celle-ci ne dispose pas d'assez de place";
+        }
+    }
+    if (strtotime($dateReservation) < time()){
+        var_dump($dateReservation);
+        var_dump(time());
+        echo "Veuillez entre une date valide s'il vous plait";
+    }else {
+        $sql = "SELECT * FROM t_reservations";
+        $reponse = $db->query($sql);
+        while ($donnees = $reponse->fetch(PDO::FETCH_ASSOC)){
+            $dateExistante = $donnees['DATE'];
+            if ($dateReservation == $dateExistante && strtotime($heureDebut) == strtotime($donnees['HEUREDEBUT']) ||$dateReservation == $dateExistante && strtotime($heurefin) == strtotime($donnees['HEUREFIN'])) {
+
+                echo "Désole le créneaux demandé est deja occupée";
+            }else{
+                $sql = "INSERT INTO `t_reservations` (`ID_RESERVATION`, `DATE`, `HEUREDEBUT`, `HEUREFIN`, `NOM`, `T_SALLES_ID_SALLE`) VALUES (NULL, '$dateReservation', '$heureDebut', '$heurefin', '$nom', '$salle')";
+                $result = $db->query($sql);
+            }
+
+        }
+    }
+}
